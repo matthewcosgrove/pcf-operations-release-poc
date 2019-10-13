65868d0480eb52fc3b57a0424c9c65b818567d3f
HZ-TU <hz-tu@dell-app.com>
2019-10-13 18:52:44 +0000

    Re-order credhub file to prep for migrating to vars

--- ci/bootstrap_populate_credhub_from_infra_settings_deprecated.sh
+++ ci/bootstrap_populate_credhub_from_infra_settings_deprecated.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -eu
 
+# Keeping in credhub as need to parse array
+credhub set -t value -n /concourse/main/testflight_vcenter_dns -v "$(bosh int --path=/vcenter_dns/0 $INPUT_VARS_YAML)"
+
 # Everything below is deprecated as the preferred approach will be to externalise the vars file to a private git repo and have the pipeline dynamically produce the config/vars.yml for all non-sensitive vars (for better auditability of environment specific config)
+
+
 credhub set -t value -n /concourse/main/testflight_vcenter_ip -v $GOVC_URL
 credhub set -t value -n /concourse/main/testflight_vcenter_datacenter -v $GOVC_DATACENTER
 REPO_ROOT_DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd ))"
 INPUT_VARS_YAML=$REPO_ROOT_DIR/ci/testflight-deployment/infra-settings.yml
+# Same name in infra-settings as in credhub so to test, MUST be deleted from credhub (as credhub interpolation happens first)
+credhub set -t value -n /concourse/main/testflight_opsman_hostname -v "$(bosh int --path=/testflight_opsman_hostname $INPUT_VARS_YAML)"
+credhub set -t value -n /concourse/main/testflight_opsman_vm_name -v "$(bosh int --path=/testflight_opsman_vm_name $INPUT_VARS_YAML)"
+
+# To be deleted from credhub but not essential for testing
 credhub set -t value -n /concourse/main/testflight_vcenter_ca_cert -v "$(bosh int --path=/vcenter_ca_cert $INPUT_VARS_YAML)" # must be type value to be used by create-vm.sh in Pivotal Platform Automation as defined in our config/opsman.yml
 credhub set -t value -n /concourse/main/testflight_vcenter_host_full_govc_path -v "$(bosh int --path=/vcenter_host_full_govc_path $INPUT_VARS_YAML)"
 credhub set -t value -n /concourse/main/testflight_vcenter_vms_full_govc_path -v "$(bosh int --path=/vcenter_vms_full_govc_path $INPUT_VARS_YAML)"
@@ -14,11 +24,8 @@ credhub set -t value -n /concourse/main/testflight_opsman_ip -v "$(bosh int --pa
 credhub set -t value -n /concourse/main/testflight_vcenter_network -v "$(bosh int --path=/network_name $INPUT_VARS_YAML)"
 credhub set -t value -n /concourse/main/testflight_vcenter_datastore -v "$(bosh int --path=/vcenter_ds $INPUT_VARS_YAML)"
 credhub set -t value -n /concourse/main/testflight_network_gateway -v "$(bosh int --path=/internal_gw $INPUT_VARS_YAML)"
-credhub set -t value -n /concourse/main/testflight_vcenter_dns -v "$(bosh int --path=/vcenter_dns/0 $INPUT_VARS_YAML)"
 credhub set -t value -n /concourse/main/testflight_vcenter_netmask -v "$(bosh int --path=/vcenter_netmask $INPUT_VARS_YAML)"
 credhub set -t value -n /concourse/main/testflight_infra_ntp -v "$(bosh int --path=/infra_ntp $INPUT_VARS_YAML)"
-credhub set -t value -n /concourse/main/testflight_opsman_hostname -v "$(bosh int --path=/testflight_opsman_hostname $INPUT_VARS_YAML)"
-credhub set -t value -n /concourse/main/testflight_opsman_vm_name -v "$(bosh int --path=/testflight_opsman_vm_name $INPUT_VARS_YAML)"
 credhub set -t value -n /concourse/main/testflight_opsman_url -v "https://$(bosh int --path=/opsman_ip $INPUT_VARS_YAML)"
 
 # config/director-template.yml
