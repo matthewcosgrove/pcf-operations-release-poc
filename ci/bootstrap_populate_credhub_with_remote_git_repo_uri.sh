5afbe991dc6c128713748a8414e0ec4190596d43
HZ-TU <hz-tu@dell-app.com>
2019-10-16 20:31:29 +0000

    Segregate the testflight state commits

--- ci/bootstrap_populate_credhub_with_remote_git_repo_uri.sh
+++ ci/bootstrap_populate_credhub_with_remote_git_repo_uri.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -eu
 credhub set -t value -n /concourse/main/pcf-operations-release/git_repo_uri -v $PCF_OPERATIONS_RELEASE_GIT_REPO_URI
+credhub set -t value -n /concourse/main/pcf-operations-release/git_testflight_state_repo_uri -v $PCF_OPERATIONS_RELEASE_TESTFLIGHT_GIT_REPO_URI
