67ebc513617e035935a53d903324fee6578ff9b9
HZ-TU <hz-tu@dell-app.com>
2019-10-16 19:36:03 +0000

    Configurable infra settings

--- ci/bootstrap_populate_credhub_with_infra_settings.sh
+++ ci/bootstrap_populate_credhub_with_infra_settings.sh
@@ -2,7 +2,8 @@
 set -eu
 
 REPO_ROOT_DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd ))"
-INPUT_VARS_YAML=$REPO_ROOT_DIR/ci/testflight-deployment/infra-settings.yml
+: "${INPUT_VARS_YAML:? INPUT_VARS_YAML must be set}"
+
 # Keeping in credhub as need to parse array
 credhub set -t value -n /concourse/main/testflight_vcenter_dns -v "$(bosh int --path=/vcenter_dns/0 $INPUT_VARS_YAML)"
 
