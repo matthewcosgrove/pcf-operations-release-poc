7cf7b6e9b02a273beecc174356863d9959d47f9f
HZ-TU <hz-tu@dell-app.com>
2019-10-16 19:22:47 +0000

    Tidy up init scripts

--- ci/bootstrap_populate_credhub_from_infra_settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-REPO_ROOT_DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd ))"
-INPUT_VARS_YAML=$REPO_ROOT_DIR/ci/testflight-deployment/infra-settings.yml
-# Keeping in credhub as need to parse array
-credhub set -t value -n /concourse/main/testflight_vcenter_dns -v "$(bosh int --path=/vcenter_dns/0 $INPUT_VARS_YAML)"
-
--- ci/bootstrap_populate_credhub_with_infra_settings.sh
+++ ci/bootstrap_populate_credhub_with_infra_settings.sh
@@ -3,6 +3,9 @@ set -eu
 
 REPO_ROOT_DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd ))"
 INPUT_VARS_YAML=$REPO_ROOT_DIR/ci/testflight-deployment/infra-settings.yml
+# Keeping in credhub as need to parse array
+credhub set -t value -n /concourse/main/testflight_vcenter_dns -v "$(bosh int --path=/vcenter_dns/0 $INPUT_VARS_YAML)"
+
 credhub set -t value -n /concourse/main/testflight_opsman_ip -v "$(bosh int --path=/opsman_ip $INPUT_VARS_YAML)"
 credhub set -t value -n /concourse/main/testflight_opsman_url -v "https://$(bosh int --path=/opsman_ip $INPUT_VARS_YAML)"
 credhub set -t value -n /concourse/main/testflight_opsman_api_username -v "$(bosh int --path=/testflight_opsman_api_username $INPUT_VARS_YAML)"
