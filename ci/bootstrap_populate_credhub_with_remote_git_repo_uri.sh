ec8e735f8c2e1dcb41170403b06996d598f42144
Matthew Cosgrove <mc.matthew.cosgrove@gmail.com>
2020-02-04 20:46:50 +0000

    Change default pipeline name

--- ci/bootstrap_populate_credhub_with_remote_git_repo_uri.sh
+++ ci/bootstrap_populate_credhub_with_remote_git_repo_uri.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
 
-PIPELINE_NAME=${PIPELINE_NAME:-pcf-automation-release}
+PIPELINE_NAME=${PIPELINE_NAME:-pcf-operations-release}
 credhub set -t value -n /concourse/main/$PIPELINE_NAME/git_repo_uri -v $PCF_OPERATIONS_RELEASE_GIT_REPO_URI
 credhub set -t value -n /concourse/main/$PIPELINE_NAME/git_testflight_state_repo_uri -v $PCF_OPERATIONS_RELEASE_TESTFLIGHT_GIT_REPO_URI
