96e7cf36a851e7cd724e8ba04eeb171e6c866ac7
matthewcosgrove <mc.matthew.cosgrove@gmail.com>
2020-02-06 11:58:27 +0000

    Add new field for 2.8 Small Footprint PAS password generation script

--- ci/bootstrap_populate_credhub_generate_ert_creds.sh
+++ ci/bootstrap_populate_credhub_generate_ert_creds.sh
@@ -2,6 +2,7 @@
 set -eu
 credhub generate -t password -n /concourse/main/testflight_ert_properties_credhub_key_encryption_passwords_0_key
 credhub generate -t password -n /concourse/main/testflight_ert_properties_cloud_controller_encrypt_key
+credhub generate -t password -n /concourse/main/testflight_ert_properties_nfs_volume_driver_enable_ldap_service_account_password
 credhub generate -t certificate -n /concourse/main/testflight_ert_properties_networking_poe_ssl_certs_0_certificate -o testflight --self-sign
 credhub generate -t certificate -n /concourse/main/testflight_ert_uaa_service_provider_key_credentials -o testflight --self-sign
 
