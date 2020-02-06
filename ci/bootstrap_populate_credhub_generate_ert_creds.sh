1215d04c3dc21e9fef4bc78352d0af9a176b668b
matthewcosgrove <mc.matthew.cosgrove@gmail.com>
2020-02-06 11:41:29 +0000

    Add new field for 2.8 Small Footprint PAS

--- ci/bootstrap_populate_credhub_generate_ert_creds.sh
+++ ci/bootstrap_populate_credhub_generate_ert_creds.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 credhub generate -t password -n /concourse/main/testflight_ert_properties_credhub_key_encryption_passwords_0_key
+credhub generate -t password -n /concourse/main/testflight_ert_properties_cloud_controller_encrypt_key
 credhub generate -t certificate -n /concourse/main/testflight_ert_properties_networking_poe_ssl_certs_0_certificate -o testflight --self-sign
 credhub generate -t certificate -n /concourse/main/testflight_ert_uaa_service_provider_key_credentials -o testflight --self-sign
 
